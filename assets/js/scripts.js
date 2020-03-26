611cb98e42f374c1d546fc66df9c4319356443d7
Ian Mihura <mihura.ian@gmail.com>
2020-03-26 15:40:50 +0100

    scroll indicator

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -73,3 +73,13 @@ $(document).ready(function() {
   $( "p.expand_end" ).hide();
 
 });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
