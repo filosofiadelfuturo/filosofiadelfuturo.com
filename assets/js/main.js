b73b195b57335c65ee9bda475af2cf691297f271
Ian Mihura <mihura.ian@gmail.com>
2020-06-05 15:26:30 +0200

    minor bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,12 @@ let _tagScrollCorrect = function () {
     if (aurl[aurl.length - 1][0] == "#")
         window.scrollBy(0, -100);
 };
+
+window.onscroll = function () { myFunction(); };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("scroll-indicator").style.width = scrolled + "%";
+}
